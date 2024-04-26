c63e0b2a6faad1cef0d1f1cdb04a24c460874ce7
Leoncio <leonciopimentelperez@gmail.com>
2024-04-26 14:09:34 -0600

    'hola'

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -83,7 +83,7 @@ class AuthController extends Controller
         if(!$validated->fails()){
             $secretkey = env('NOCAPTCHA_SECRETKEY');
             $recatcha = $request->input('g-recaptcha-response');
-            $response = Http::post("https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$recatcha");
+            $response = Http::post("https://www.google.com/recaptcha/api/siteverify?secret=6LdVV2gpAAAAAP6JVBeKgtRYp41sgCN2a-qta7os&response=$recatcha");
 
             if(!$response['success']){
                 $validated->errors()->add(
